baca7d711bc0e1a1959b8534cb73b4f1b8585a8d
Lucia Cerchie <luciacerchie@gmail.com>
2022-12-06 12:09:18 -0700

    day5 first answer

--- day-05/index.js
+++ day-05/index.js
@@ -1,4 +1,5 @@
 const fs = require('node:fs')
+const { type } = require('node:os')
 const readline = require('node:readline')
 
 // [P]     [L]         [T]
@@ -14,15 +15,17 @@ const readline = require('node:readline')
 async function processLineByLine() {
     const fileStream = fs.createReadStream('./day-05/input.txt')
 
-    let stack1 = ['P', 'L', 'M', 'N', 'W', 'V', 'B', 'H']
-    let stack2 = ['H', 'Q', 'M']
-    let stack3 = ['L', 'M', 'Q', 'F', 'G', 'B', 'D', 'N']
-    let stack4 = ['G', 'W', 'M', 'Q', 'F', 'T', 'Z']
-    let stack5 = ['P', 'H', 'T', 'M']
-    let stack6 = ['T', 'G', 'H', 'D', 'J', 'M', 'B', 'C']
-    let stack7 = ['R', 'V', 'F', 'B', 'N', 'M']
-    let stack8 = ['S', 'G', 'R', 'M', 'H', 'L', 'P']
-    let stack9 = ['N', 'C', 'B', 'D', 'P']
+    let stackObject = {
+        stack1: ['H', 'B', 'V', 'W', 'N', 'M', 'L', 'P'],
+        stack2: ['M', 'Q', 'H'],
+        stack3: ['N', 'B', 'D', 'G', 'F', 'Q', 'M', 'L'],
+        stack4: ['Z', 'T', 'F', 'Q', 'M', 'W', 'G'],
+        stack5: ['M', 'T', 'H', 'P'],
+        stack6: ['C', 'B', 'M', 'J', 'D', 'H', 'G', 'T'],
+        stack7: ['M', 'N', 'B', 'V', 'F', 'R'],
+        stack8: ['P', 'L', 'H', 'M', 'R', 'G', 'S'],
+        stack9: ['P', 'D', 'B', 'C', 'N'],
+    }
 
     const rl = readline.createInterface({
         input: fileStream,
@@ -32,6 +35,7 @@ async function processLineByLine() {
     // ('\r\n') in input.txt as a single line break.
 
     for await (const line of rl) {
+        console.log('LINE', line)
         // Each line in input.txt will be successively available here as `line`.
         let destinationColumn = +line.charAt(line.length - 1)
 
@@ -40,7 +44,7 @@ async function processLineByLine() {
         let numberToMove =
             typeof +choppedString.charAt(1) === 'number'
                 ? +(choppedString.charAt(0) + choppedString.charAt(1))
-                : choppedString.charAt(0)
+                : +choppedString.charAt(0)
 
         let finalChop =
             typeof +choppedString.charAt(1) === 'number'
@@ -58,8 +62,35 @@ async function processLineByLine() {
         console.log('NUMBER', numberToMove)
         console.log('SOURCE', sourceColumn)
         console.log('DESTINATION', destinationColumn)
-        console.log(`Line from file: ${line}`)
+
+        for (let i = 0; i < numberToMove; i++) {
+            if (Object.keys(stackObject).includes(`stack${sourceColumn}`)) {
+                console.log('so before', stackObject)
+
+                let returnedLetterArray = stackObject[`stack${sourceColumn}`]
+                let letterToAdd = returnedLetterArray.pop()
+
+                let destinationArray = stackObject[`stack${destinationColumn}`]
+
+                destinationArray.push(letterToAdd)
+                console.log('so after', stackObject)
+            }
+            letLastLetterInEachArray = `${
+                stackObject[`stack${1}`][stackObject[`stack${1}`].length - 1]
+            }${stackObject[`stack${2}`][stackObject[`stack${2}`].length - 1]}${
+                stackObject[`stack${3}`][stackObject[`stack${3}`].length - 1]
+            }${stackObject[`stack${4}`][stackObject[`stack${4}`].length - 1]}${
+                stackObject[`stack${5}`][stackObject[`stack${5}`].length - 1]
+            }${stackObject[`stack${6}`][stackObject[`stack${6}`].length - 1]}${
+                stackObject[`stack${7}`][stackObject[`stack${7}`].length - 1]
+            }${stackObject[`stack${8}`][stackObject[`stack${8}`].length - 1]}${
+                stackObject[`stack${9}`][stackObject[`stack${9}`].length - 1]
+            }`
+            console.log(letLastLetterInEachArray)
+        }
     }
 }
 
+// not right answer: QHBZMCMPP
+// not right answer: TMWBHFGMD
 processLineByLine()
